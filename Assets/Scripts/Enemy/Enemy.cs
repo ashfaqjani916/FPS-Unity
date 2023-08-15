b1f8505870a92ff76c3b782662290aec78685d4f
ashfaqjani916 <ashfaqjani916@gmail.com>
2023-08-16 00:31:45 +0530

    added attack feature for enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,22 +9,59 @@ public class Enemy : MonoBehaviour
     private StateMachine stateMachine;
     private NavMeshAgent agent;
     public NavMeshAgent Agent{ get => agent;}
+    public GameObject Player {get => player;}
 
     //this is just for debugging purposes
     [SerializeField]
     private string currentState;
     public Path path;
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float FOV = 85f;
+
+    [Header("Weapon values")]
+    public Transform gunBarrel;
+    [Range(0.1f,10f)]
+    public float fireRate;
+
     // Start is called before the first frame update
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CanSeePlayer();
+        currentState = stateMachine.activeState.ToString();
+    }
+    public bool CanSeePlayer()
+    {
+        if(player != null)
+        {
+            //is the player close enough to be seen
+            if(Vector3.Distance(transform.position,player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position;
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if(angleToPlayer >= -FOV && angleToPlayer <= FOV)
+                {
+                    Ray ray = new Ray(transform.position , targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    if(Physics.Raycast(ray,out hitInfo,sightDistance))
+                    {
+                        if(hitInfo.transform.gameObject == player)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/Enemy/States/AttackState.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : BaseState
+{
+    private float moveTimer;
+    private float losePlayerTimer;
+    private float shotTimer;
+    public override void Enter()
+    {
+    }
+    public override void Perform()
+    {
+        if(enemy.CanSeePlayer())
+        {
+            losePlayerTimer = 0;
+            moveTimer += Time.deltaTime;
+            shotTimer += Time.deltaTime;
+            enemy.transform.LookAt(enemy.Player.transform);
+
+            if(shotTimer >enemy.fireRate)
+            {
+                Shoot();
+            }
+            if(moveTimer > Random.Range(3,7))
+            {
+                enemy.Agent.SetDestination(enemy.transform.position + (Random.insideUnitSphere *5) );
+                moveTimer = 0;
+            }
+        }
+        else
+        {
+            losePlayerTimer += Time.deltaTime;
+            if(losePlayerTimer > 8)
+            {
+                stateMachine.ChangeState(new PatrolState());
+            }
+        }
+    }
+    public override void Exit()
+    {
+    }
+    
+    public void Shoot()
+    {
+        //store the reference to the gun barrel 
+        Transform gunbarrel = enemy.gunBarrel;
+        // instantiate a new bullet 
+        GameObject bullet = GameObject.Instantiate(Resources.Load("bullet 1") as GameObject,gunbarrel.position,enemy.transform.rotation);
+        // calculate the direction to the player
+        Vector3 shootDirection = (enemy.Player.transform.position - gunbarrel.transform.position).normalized;
+        //add force rigidbody of the bullet.
+        bullet.GetComponent<Rigidbody>().velocity = Quaternion.AngleAxis(Random.Range(-1f,1f),Vector3.up) * shootDirection *40;
+        Debug.Log("Shoot");
+        shotTimer=0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
